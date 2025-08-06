48ac6d482d61489c8f9a8df90f5d4a279f2053e0
Kağan İnan <kagan@kaizentech.net>
2025-08-06 17:12:27 +0300

    update image size

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import RecipeList from './pages/RecipeList';
 import RecipeDetail from './pages/RecipeDetail';
